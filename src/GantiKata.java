0d7c018c7ef0e5b57e084db156257570dc42e637
alvinlukmanNH <galinx20gmail.com>
2018-10-01 22:56:40 +0700

    GantiKata

---
+++ src/GantiKata.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alvin
+ */
+public class GantiKata {
+    public static void main(String[] args) {
+       
+ //deklarasi Variabel
+        String kalimatAwal;
+        String kalimatBaru;
+        String gantiKata;
+        String jadiKata;
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("====Program Ganti Kata====\n");
+        System.out.print("Masukkan Kalimat : ");
+        kalimatAwal = scn.nextLine();
+        System.out.print("Ganti Kata : ");
+        gantiKata = scn.next();
+        System.out.print("Menjadi Kata : ");
+        jadiKata = scn.next();
+        
+//        proses pergantian kata dengan kata yang lain
+        kalimatBaru =  (kalimatAwal).replaceAll(gantiKata, jadiKata);
+        
+//        menampilkan hasil perubahan kata
+        System.out.println("\n====Hasil Formatting====");
+        System.out.println("Kalimat Awal : "+kalimatAwal);
+        System.out.println("Kalimat Baru : "+kalimatBaru);
+        
+        
+    }
+    
+}
